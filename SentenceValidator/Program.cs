66f56a48041ab61141f705391097479393fa963d
tpurdon01 <tpurdon01@qub.ac.uk>
2022-05-10 17:20:30 +0100

    Initial Commit

---
+++ SentenceValidator/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, and welcome to the sentence validator!");
+Console.WriteLine("Please enter the sentence you would like to validate below:");
+try
+{
+    var sentenceToValidate = Console.ReadLine();
+    if (sentenceToValidate is not null || sentenceToValidate != string.Empty)
+    {
+        var sentenceValidator = new SentenceValidator.SentenceValidator(sentenceToValidate);
+    }
+    else
+    {
+        throw new Exception();
+    }
+} catch(Exception ex)
+{
+    Console.WriteLine($"Error! {0}", ex.Message);
+}
+
---
+++ SentenceValidator/SentenceValidator.cs
@@ -0,0 +1,50 @@
+﻿namespace SentenceValidator
+{
+	public class SentenceValidator
+	{
+		private readonly string sentence;
+
+		public SentenceValidator(string sentence)
+		{
+            this.sentence = sentence;
+		}
+
+		// change this so method returns a validation message
+		public bool IsSentenceValid()
+        {
+			var listOfWords = DecomposeSentenceToList(this.sentence);
+			if (StringStartsWithCapital(listOfWords.FirstOrDefault()) &&
+				ListEndsWithPeriod(listOfWords) &&
+				ListHasEvenQuotations(listOfWords) &&
+				IsCorrectNumberFormat(listOfWords))
+				return true;
+			return false;
+        }
+
+		private bool StringStartsWithCapital(string word)
+        {
+			return false;
+        }
+
+		private bool ListEndsWithPeriod(List<string> sentence)
+        {
+			return false;
+        }
+
+		private bool ListHasEvenQuotations(List<string> sentence)
+        {
+			return false;
+        }
+
+		private bool IsCorrectNumberFormat(List<string> sentence)
+        {
+			return false;
+        }
+
+		private List<string> DecomposeSentenceToList(string sentance)
+        {
+			return new List<string> { "" };
+        }
+	}
+}
+
